3fd6f722519b7b634a65fcaa23d75ac121221724
Daniel Kaleb <danielkallebmb@gmail.com>
2024-06-03 17:20:09 -0300

    Perfil 03/06

--- client/src/views/Perfil/index.jsx
+++ client/src/views/Perfil/index.jsx
@@ -1,35 +1,22 @@
 import React from 'react'
-<link rel="stylesheet" href="css/normalize.css"/>
+import './stylllo.css';
 
 
 
 function Perfil() {
   return (
-    <div>
-      <div class="navegacao">
-        <div class="area_menu">
-            <h1 class="logo">Hive</h1>
-            <div class="menu">
-                <a href="tela_perfil.html"></a>
-                <a href="tela_perfil.html"></a>
-                <a href="tela_perfil.html"></a>
-            </div>
-        </div>
-    </div>
-    <div class="perfil">
-        <div class="postado">            
-            <p class="container"> NOME</p>     
-            <p class="container_2">NASCIMENTO</p>
-            <p class="container_3">UNIVERSIDADE</p>
-            <p class="container_4">E-MAIL</p>
-            <p class="container_5">TELEFONE</p>
-
-            <p></p>        
-        </div>
+    <div className="container">
+      <div className="barracima">
+        <h1>Hive</h1>
+        <h2>Anuncie uma republica</h2>
+      </div>
+      <div className="img">
+      </div>
+      <div className="infos">
         
-
+      </div>
     </div>
-    </div>
+    
   )
 }
 
