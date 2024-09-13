235833d5c05c2bbad439d5d8657fda2c695500f6
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-09-13 16:33:52 -0300

    inicio tela cadastro

--- client/src/views/Cadastro/Cadastro.jsx
+++ client/src/views/Cadastro/Cadastro.jsx
@@ -1,53 +1,42 @@
 import React from 'react'
 import './Cad.css';
-import user from "./img/user_icon.png"
-import email from "./img/email.png"
-import senha from "./img/senha.png"
-import phone from "./img/fone.png"
-import data from "./img/data.png"
 
 function Cadastro() {
   return (
-    <div className="container-cad">
-      <div className="header">
-        <div className="texto">Cadastrar-se</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        <div className="input">
-          <img src={user} />
-          <input type="text" placeholder='Nome' required />
+    <div>
+      <main className='conteudo-site'>
+        <div className='tela-inter'>
+          <div id='target' className='geral' role='group'>
+            <div className='box'>
+              <header id='box-header' className='headerCSS'>
+                <div className='esp'></div>
+                  <section>
+                    <h1>Entrar ou Cadastrar-se</h1>
+                  </section>
+                <div className='esp'></div>
+              </header>
+              <div className='corpo' aria-labelledby="box-header">
+                <div>
+                  <div className='boasVindas'>
+                    <h2 className='boasTXT'>Bem-vindo ao Hive</h2>
+                  </div>
+                  <form action="/authenticate" method='POST' noValidate data-testid="auth-form">
+                    <div className='cont-campos'>
+                      <input type="hidden" name='from' value='' />
+                      <input type="hidden" name="airlock_id" id="" />
+                      <div className='campos'>
+                        <div className='primeiro-campo'>
+                          <input type="text" />
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="input">
-          <img src={user} />
-          <input type="text" placeholder='Sobrenome'  required/>
-        </div>
-        <div className="input">
-          <img src={email} alt="" />
-          <input type="email" placeholder='E-mail' required/>
-        </div>
-        <div className="input">
-          <img src={data} alt="" />
-          <input type="date" />
-        </div>
-        <div className="input">
-          <img src={phone} alt="" />
-          <input type="tel" placeholder='Núm. Telefone'/>
-        </div>
-        <div className="input">
-          <img src={senha} alt="" />
-          <input type="password" placeholder='Senha' required/>
-        </div>
-        <div className="input">
-          <img src={senha} alt="" />
-          <input type="password" placeholder='Repita a senha' required/>
-        </div>
-      </div>
-      <div className="forgot-pass">Esqueci minha senha? <span>Clique aqui.</span></div>
-      <div className="submit-container-cad">
-        <div className="submit">Cadastrar-se</div>
-        <div className="submit">Entrar</div>
-      </div>
+      </main>
     </div>
   )
 }
