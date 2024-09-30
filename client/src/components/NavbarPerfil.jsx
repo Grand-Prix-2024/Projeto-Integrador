054b5f28f0bbcfa8d80f081d446c086477d5fc67
DANIEL KALEB DE O. ALVES MOREIRA <d.moreira@labcetec.net>
2024-09-30 15:51:48 -0300

    09_30

--- client/src/components/NavbarPerfil.jsx
+++ client/src/components/NavbarPerfil.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import Botao from "./Botao";
 
 
 function NavbarPerfil() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+        setButton(false);
+    } else {
+        setButton(true);
+    }
+  };
+
+  
+
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
             <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3"></i>
+            HIVE <i className="fab fa-typo3"></i>
             </Link>
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times': 'fas fa-bars'}/>
@@ -23,16 +37,22 @@ function NavbarPerfil() {
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/favoritos' className='nav-links' onClick={closeMobileMenu}>
                     Favoritos
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/chat' className='nav-links' onClick={closeMobileMenu}>
                     Chat
                     </Link>
                 </li>
+                <li className='nav-item'>
+                    <Link to='/registrar' className='nav-links-mobile' onClick={closeMobileMenu}>
+                    Registrar
+                    </Link>
+                </li>
             </ul>
+            {button && <Botao buttonStyle='btn--outline'>REGISTRAR</Botao>}
         </div>
     </nav>
     </>
