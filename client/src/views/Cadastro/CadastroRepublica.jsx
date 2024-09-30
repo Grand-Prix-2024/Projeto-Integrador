eba0263220555c4f43a1597555a122773bb1bdcd
GABRIEL DE ASSIS SPERANDIO <g.sperandio@labcetec.net>
2024-09-30 15:53:00 -0300

    Fazendo css

--- client/src/views/Cadastro/CadastroRepublica.jsx
+++ client/src/views/Cadastro/CadastroRepublica.jsx
@@ -22,102 +22,109 @@ function CadastroRepublica() {
                         <p>O morador dorme em um quarto que pode ser compartilhado com outras pessoas</p>
                     </button>
                 </div>
+                
                 <h2>Compartilhe mais informações essenciais</h2>
-                <div class="contador">
-                    <h4>
-                        Moradores
-                        <button id="menos">-</button>
-                        <span id="quantidade">0</span>
-                        <button id="mais">+</button>
-                    </h4>
-                    <h4>
-                        Quartos
-                        <button id="menos">-</button>
-                        <span id="quantidade">0</span>
-                        <button id="mais">+</button>
-                    </h4>
-                    <h4>
-                        Banheiros
-                        <button id="menos">-</button>
-                        <span id="quantidade">0</span>
-                        <button id="mais">+</button>
-                    </h4>
-                    <h4>
-                        Camas
-                        <button id="menos">-</button>
-                        <span id="quantidade">0</span>
-                        <button id="mais">+</button>
-                    </h4>
+                {/* Moradores */}
+                <div class="info-container">
+                    <span class="label">Moradores</span>
+                    <button class="btnQtd" onclick="updateQuantidade('moradores', 1)">+</button>
+                    <span id="moradores" class="quantidade">3</span>
+                    <button class="btnQtd" onclick="updateQuantidade('moradores', -1)">-</button>
                 </div>
+
+                {/* Quartos */}
+                <div class="info-container">
+                    <span class="label">Quartos</span>
+                    <button class="btnQtd" onclick="updateQuantidade('quartos', 1)">+</button>
+                    <span id="quartos" class="quantidade">2</span>
+                    <button class="btnQtd" onclick="updateQuantidade('quartos', -1)">-</button>
+                </div>
+
+                {/* Banheiros */}
+                <div class="info-container">
+                    <span class="label">Banheiros</span>
+                    <button class="btnQtd" onclick="updateQuantidade('banheiros', 1)">+</button>
+                    <span id="banheiros" class="quantidade">1</span>
+                    <button class="btnQtd" onclick="updateQuantidade('banheiros', -1)">-</button>
+                </div>
+
+                {/* Camas */}
+                <div class="info-container">
+                    <span class="label">Camas</span>
+                    <button class="btnQtd" onclick="updateQuantidade('camas', 1)">+</button>
+                    <span id="camas" class="quantidade">4</span>
+                    <button class="btnQtd" onclick="updateQuantidade('camas', -1)">-</button>
+                </div>
+                
                 <div>
                     <h3>Etapa 2</h3>
                     <h1>Faça sua república se destacar</h1>
                     <p>Adcione elementos para tornar sua acomodação mais interessante</p>
 
                     <h2>Informe o que sua moradinha tem a mais para oferecer</h2>
-                    <button>
+                    <button class="btn">
                         Wi-fi
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Televisão
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Cozinha
                         <i></i> {/* icone */}
                     </button>
                     <br></br>
-                    <button>
-                       Ar-condicionado
+                    <button class="btn">
+                        Ar-condicionado
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Canto de estudo
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Chuveiro-elétrico
                         <i></i> {/* icone */}
                     </button>
 
                     <h2>Informe comodidades da sua moradia</h2>
-                    <button>
+                    <button class="btn">
                         Piscina
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Churrasqueira
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Academia
                         <i></i> {/* icone */}
                     </button>
                     <br></br>
-                    <button>
-                       Varanda
+                    <button class="btn">
+                        Varanda
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Jardim
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Banheira
                         <i></i> {/* icone */}
                     </button>
 
                     <h2>Itens de seguranca</h2>
-                    <button>
+                    <button class="btn">
                         Extintor
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Cameras
                         <i></i> {/* icone */}
                     </button>
-                    <button>
+                    <button class="btn">
                         Alarmes
                         <i></i> {/* icone */}
                     </button>
@@ -134,7 +141,7 @@ function CadastroRepublica() {
                     </div>
                     <h2>Nomeie sua moradia para o anúncio</h2>
                     <p>use sua criatividade!</p>
-                    <input type="text" placeholder='Ex.: União Twink'/>
+                    <input type="text" placeholder='Ex.: União Twink' />
                     <h2>Determine seu valor</h2>
                     <p>você podera alterar quando quiser</p>
                     <h1>R$ <input type="number" placeholder='70,00' /></h1>
@@ -144,17 +151,17 @@ function CadastroRepublica() {
                     <h3>Etapa 4</h3>
                     <h1>Defina seu endereço</h1>
                     <p>configure a localizacao da acomodacao para ser encontrada.
-                    os detalhes seram revelados apenas depois da negociacao</p>
+                        os detalhes seram revelados apenas depois da negociacao</p>
 
                     <form action=""><input type="text" placeholder='País' /></form>
                     <br />
                     <form action="">
-                    <input type="text" placeholder='CEP'/> <br />
-                    <input type="text" placeholder='Endereço'/> <br />
-                    <input type="text" placeholder='Bairro'/> <br />
-                    <input type="text" placeholder='Cidade/Município'/> <br />
-                    <input type="text" placeholder='Estadual'/> <br />
-                    <input type="text" placeholder='Apartamento'/> <br />
+                        <input type="text" placeholder='CEP' /> <br />
+                        <input type="text" placeholder='Endereço' /> <br />
+                        <input type="text" placeholder='Bairro' /> <br />
+                        <input type="text" placeholder='Cidade/Município' /> <br />
+                        <input type="text" placeholder='Estadual' /> <br />
+                        <input type="text" placeholder='Apartamento' /> <br />
                     </form>
                 </div>
                 <div>
