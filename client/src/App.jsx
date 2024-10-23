7e37efd1f1d75085043d88a9e97d2a55ea46b560
DANIEL KALEB DE O. ALVES MOREIRA <d.moreira@labcetec.net>
2024-10-23 16:21:52 -0300

    merger main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,13 @@ import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import {Routes} from "react-router-dom";
 import {Route} from 'react-router-dom';
-<<<<<<< HEAD:client/src/App.jsx
-import Cadastro from './views/Cadastro/Cadastro.jsx';
-import Perfil from './views/Perfil/Perfil.jsx';
-import NavbarPerfil from './components/NavbarPerfil.jsx';
-=======
-import Cadastro from './views/Cadastro/Cadastro';
 
+import Cadastro from './views/Cadastro/Cadastro';
 import Perfil from './views/Perfil/Perfil.jsx'
 import CadastroRepublica from './views/Cadastro/CadastroRepublica';
 
 
->>>>>>> 61da3eadfb804c27419bd410a7927476e5ad5760:client/src/App.js
+
 
 function App() {
   return (
