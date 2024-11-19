b07a7af10c222a4861bbd541ea63a7a54c7e7613
DANIEL KALEB DE O. ALVES MOREIRA <d.moreira@labcetec.net>
2024-11-19 16:12:27 -0300

    perfil

--- client/src/views/Perfil/Perfil.jsx
+++ client/src/views/Perfil/Perfil.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import './styles.module.css'
-import PerfilHeader from './PerfilHeader.jsx';
 import { Card, Button, Image, CardText } from 'react-bootstrap';
 
 
@@ -9,7 +7,7 @@ function Perfil() {
   return (
    <>
     <Navbar/>
-    <Card style={{ width: '500px', height:'300px' ,  padding: '50px', margin:'50px', borderRadius:'20px' }}>
+    <Card style={{ width: '500px', height:'400px' ,  padding: '50px', margin:'50px', borderRadius:'20px' }}>
       <Image 
         src="https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2024/09/12114930/rapper-travis-scott-confirmado-atracao-rock-in-rio-scaled-1-620x620.webp" // Substitua pela URL real da imagem de perfil
         roundedCircle 
@@ -34,31 +32,47 @@ function Perfil() {
         </Card.Text>
       </Card.Body>
     </Card>
-    <Card style={{ width: '500px', height:'400px' ,  padding: '50px', margin:'50px', borderRadius:'20px' }}>
+    <Card style={{ width: '500px', height:'300px' ,  padding: '20px', margin:'50px', borderRadius:'20px' }}>
       <Card.Body>
         <Card.Title> </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+        {/* <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle> */}
         <Card.Text>
-          <div className="d-flex justify-content-between">
-            <span></span>
-            <span></span>
+          <div style={{marginBottom:'-30px'}} className="d-flex ">
+            <span style={{marginBottom:'50px'}}>
+              <h5>E-MAIL</h5>
+              <h6>danielkallebmb@gmail.com</h6>
+            </span>     
           </div>
-          <div className="d-flex justify-content-between">
-            <span></span>
-            <span></span>
+          <div className="d-flex ">
+            <span  style={{marginBottom:'20px'}}>
+              <h5>TELEFONE</h5>
+              <h6>(027) 98892-0342</h6>
+              </span>      
           </div>
-          <div className="d-flex justify-content-between">
-            <span></span>
-            <span></span>
+          <div className="d-flex ">
+            <span>
+              <h5>INSTAGRAM</h5>
+              <h6>@travisscott</h6>
+            </span>
+            
           </div>
         </Card.Text>
       </Card.Body>     
     </Card>
-    <Button style={{ margin:'50px', marginTop:'-20px'}} variant="btn btn-danger">Denunciar perfil</Button>
+    <Button style={{ margin:'50px', marginTop:'0px'}} variant="btn btn-danger">Denunciar perfil</Button>
     <Card className='position-absolute top-50 start-50"' style={{ width: '800px', height:'450px', marginLeft:'650px', marginBlockStart:'-370px', border:'none'}}>
       <Card.Text>
         <div>
-          <span></span>
+          <h1>JACQUES BERMAN WEBSTER II</h1>
+          <h5 style={{fontStyle:'italic'}}>ELA/DELA</h5>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam repellendus, quos ipsa voluptatum alias tempore. Tenetur reiciendis omnis ipsa autem, 
+            ullam expedita mollitia ut dolore sequi commodi vero exercitationem sapiente.
+            </span>
+            <br />
+            <br />
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet et, deleniti minus mollitia soluta ducimus sint odio repudiandae corrupti corporis 
+              magnam molestiae, iusto necessitatibus, vitae laudantium odit. Voluptatum, iste atque.
+              </span>
         </div>
       </Card.Text>
     </Card>
@@ -77,7 +91,7 @@ function Perfil() {
         </div>
       </Card.Text>
     </Card>
-    <Card className='position top-50 start-50"' style={{ width: '300px', height:'820px', marginLeft:'1550px', marginBlockStart:'-818px', marginTop:'-300px', borderRadius:'20px'}}>
+    <Card className='position top-50 start-50"' style={{ width: '300px', height:'820px', marginLeft:'1550px', marginBlockStart:'-818px',  borderRadius:'20px'}}>
       <Card.Text>
         <div>
           <span></span>
